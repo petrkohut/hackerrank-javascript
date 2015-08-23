1f0ac992f91aae9b6c78bb2a293544989fbfd078
Petr Kohut <petr.kohut@socialbakers.com>
2015-08-23 11:35:45 +0200

    update

--- algorithms/graph-theory/dijkstra-shortest-reach-2.js
+++ algorithms/graph-theory/dijkstra-shortest-reach-2.js
@@ -2,13 +2,16 @@ function buildGraphFromInput(N, input) {
     var graph = { vertices: [] };
     
     for (var i = 0; i < N; i++) {
-        graph.vertices.push({edges: [], visited: false, distance: 0});
+        graph.vertices.push({edges: [], edgeLength: [], visited: false, distance: 0});
     }
     for (var i = 0; i < input.length; i++) {
         var v1 = parseInt(input[i].split(' ')[0]);
         var v2 = parseInt(input[i].split(' ')[1]);
+        var l = parseInt(input[i].split(' ')[2]);
         graph.vertices[v1 - 1].edges.push(v2);
         graph.vertices[v2 - 1].edges.push(v1);
+        graph.vertices[v1 - 1].edgeLength.push(l);
+        graph.vertices[v2 - 1].edgeLength.push(l);
     }
     return graph;
 }
@@ -23,10 +26,15 @@ function findShortestReach(graph, startNode) {
     while (item = queue.shift()) {
         var vertice = graph.vertices[item - 1];
         for (var i = 0; i < vertice.edges.length; i++) {
-            if (!graph.vertices[vertice.edges[i] - 1].visited) {
+            var updatedDistance = false;
+            if ((graph.vertices[vertice.edges[i] - 1].distance > (vertice.edgeLength[i] + vertice.distance)) ||
+                (graph.vertices[vertice.edges[i] - 1].distance === 0)) {
+                updatedDistance = true;
+                graph.vertices[vertice.edges[i] - 1].distance = vertice.edgeLength[i] + vertice.distance;
+            }
+            if (!graph.vertices[vertice.edges[i] - 1].visited || updatedDistance) {
                 queue.push(vertice.edges[i]);
                 graph.vertices[vertice.edges[i] - 1].visited = true;
-                graph.vertices[vertice.edges[i] - 1].distance = vertice.distance + 6;
             }
         }
     }
@@ -55,7 +63,7 @@ function processData(input) {
         index += 1;
         var graph = buildGraphFromInput(N, graphInput);
         console.log(findShortestReach(graph, S));
-    }
+    } 
 } 
 
 process.stdin.resume();
